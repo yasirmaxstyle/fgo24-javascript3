4c3076d09948c869a8e41d7219441d1995584405
yasirmaxstyle <yasirmu77@gmail.com>
2025-04-21 13:57:14 +0700

    edit chaining

--- minitask3.js
+++ minitask3.js
@@ -2,4 +2,4 @@ const animal = "ayam,bebek,sapi,unta"
 //chaining built-in method
 //output "ayam,bebek,domba,unta
 
-animal.split(',').with(2, "domba")
+console.log(animal.split(',').with(2, "domba").join())

fca43a0e3638b1c68c919a3279a4df6aa97d58ba
yasirmaxstyle <yasirmu77@gmail.com>
2025-04-22 10:06:01 +0700

    add async handle

--- minitask5.js
+++ minitask5.js
@@ -9,18 +9,38 @@ function printQueue(name, delay) {
     })
 }
 
-printQueue('John', 1500)
-    .then(name => {
-        console.log(name)
-        printQueue('Ed', 2000).
-            then(name => {
-                console.log(name)
-                printQueue('Jane', 500).
-                    then(name => {
-                        console.log(name)
-                    })
-            })
-    }).
-    catch(err => {
-        console.log(err)
-    })
+// console.time
+
+// printQueue('John', 1500)
+//     .then(name => {
+//         console.log(name)
+//         printQueue('Ed', 2000).
+//             then(name => {
+//                 console.log(name)
+//                 printQueue('Jane', 500).
+//                     then(name => {
+//                         console.log(name)
+//                     })
+//             })
+//     }).
+//     catch(err => {
+//         console.log(err)
+//     })
+
+
+
+async function handleAsync() {
+    try {
+        const john = await printQueue('John', "eee")
+        console.log(john)
+        const ed = await printQueue('Ed', 2000)
+        console.log(ed)
+        const jane = await printQueue('Jane', 500)
+        console.log(jane)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+handleAsync()
+
